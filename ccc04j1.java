9fb9c2e754504cf059c67b6de8af44aae5544a18
Yiyi Xu <86274083+VorticLL@users.noreply.github.com>
2021-06-21 22:19:56 -0400

    Create ccc04j1.java

---
+++ ccc04j1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class CCC04J1
+{
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    System.out.println("The largest square has side length " + (int)Math.sqrt(n) + ".");
+  }
+}
